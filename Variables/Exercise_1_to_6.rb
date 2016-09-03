5d71bd43e2541f23984719aa1bfafd8c46038368
Sissy Wang <sissy.wang@outlook.com>
2016-09-03 11:12:09 -0500

    Variable Exerises

---
+++ Variables/Exercise_1_to_6.rb
@@ -0,0 +1,41 @@
+#1 name.rb
+puts "what's your name ?"
+a=gets.chomp
+puts "Hello #{a}"
+
+#2  age.rb
+puts "How old are you?"
+a=gets.chomp.to_i
+puts "In 10 years you will be:" 
+puts a+10
+puts "In 20 years you will be:"
+puts a+20
+puts "In 30 years you will be:"
+puts a+30
+puts "In 40 years you will be:"
+puts a+40
+
+#3
+puts "what's your name ?"
+a=gets.chomp
+puts "Hello #{a}"
+
+10.time do 
+  puts a 
+end 
+
+#4
+puts "what's your first name?"
+a=gets.chomp
+puts "what's your last name?"
+b=gets.chomp
+puts a+" "+b
+
+#5
+x=3 in first question
+error in the second question ,the variable x is undefined, it is created within the scope of do...end 
+
+#6
+In the third line, there is an undefined variable called shoes, or it is out side the scope
+
+
