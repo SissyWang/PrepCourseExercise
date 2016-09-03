edb858a9a7427d1037098f08800c04275774058c
Sissy Wang <sissy.wang@outlook.com>
2016-09-02 22:10:34 -0500

    exercise 3-7 added

---
+++ The_Basics/Exercise_3_to_7.rb
@@ -0,0 +1,30 @@
+# 3. 
+Movies = {Harry_Potter:2000,
+          Slepping_Beauty:1992,
+          Snow_white:1998,
+          Twilight:2010
+
+}
+
+puts Movies.values
+
+
+
+#4
+array=[2000,1992,1998,2010]
+array.each do |x| puts x  end 
+
+
+#5
+puts 5*4*3*2*1
+puts 6*5*4*3*2*1
+puts 7*6*5*4*3*2*1
+puts 8*7*6*5*4*3*2*1
+
+#6
+puts 2.34*2.87
+puts 1.09*8.98
+puts 3.87*8.92
+
+#7
+In the second line of my code, I suppose to use }, but I used )
