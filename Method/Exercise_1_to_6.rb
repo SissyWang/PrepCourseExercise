44effe4da6e1f7c5a51be30756b9d5e5d3a202c8
Sissy Wang <sissy.wang@outlook.com>
2016-09-03 11:44:33 -0500

    Exercises in Method chapter

---
+++ Method/Exercise_1_to_6.rb
@@ -0,0 +1,36 @@
+#1
+def greeting(name)
+  puts "this is a string for: "+name
+end 
+
+puts greeting("Sissy")
+
+#2
+1. 2
+2. nil
+3. "Joe"
+4. "four"
+5. nil
+
+#3
+def multiply(a,b)
+  puts a*b
+end 
+
+puts multiply(2,3)
+
+#4
+will not print anything, code after "return" will not get printed out
+
+#5
+1.
+def scream(words)
+  puts words+"!!!!"
+end
+
+scream("Yippeee")
+
+2. return: nil
+
+#6
+In line 1, there is a method called calculate_product,there should be 2 arguments but we only have 1
